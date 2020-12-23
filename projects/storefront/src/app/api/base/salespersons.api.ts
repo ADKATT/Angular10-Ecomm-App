002e273bdaebdc227f271adf7535e71a70c455b4
expertdeveloper001 <expertdeveloper001@gmail.com>
2020-12-23 13:21:24 +0530

    Create and edit flash messages

--- projects/storefront/src/app/api/base/salespersons.api.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export abstract class SalespersonsApi {
-
-    abstract validateSalesMessage(): Observable<void>;
-
-    // abstract signUp(email: string, password: string): Observable<User>;
-
-}
